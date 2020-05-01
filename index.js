b4f6db8a2455aa7bf8769017f524aa8480871ab1
Azucena Herrera <azucenaphx@gmail.com>
2020-04-30 18:16:23 -0700

    user input working

--- index.js
+++ index.js
@@ -1,42 +1,60 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
- 
-//ask for github user name and pull github repo
+const axios = require("axios");
 
+//ask for github user name and pull github repo
+// async function getGitURL() {
+//   try {
+//     const { userName } = await inquirer.prompt({
+//       message: "What's your GitHub username?",
+//       name: "userName"
+//     });
+
+//     const { data } = await axios.getGitURL(
+//       `https://api.github.com/users/${userName}`
+//     )
+//   }
+// }
 
 ///get user information, prompt user to answer and put together the readmeFile
-const writeFileAsync = util.promisify(fs.writeFile);
+// const writeFileAsync = util.promisify(fs.writeFile);
 
-function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "Project name:"
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Write project description"
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "What needs to be installed to successfully run your program?"
-    },
-    //confirm what they mean by usage. 
-    {
-      type: "input",
-      name: "usage",
-      message: "How will your program be used?"
-    },
-    {
+async function promptUser() {
+  try {
+    return await inquirer.prompt([
+      {
+        type: "input",
+        message: "What's your GitHub username?",
+        name: "userName"
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "Project name:"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Write project description"
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "What needs to be installed to successfully run your program?"
+      },
+      //confirm what they mean by usage. 
+      {
+        type: "input",
+        name: "usage",
+        message: "How will your program be used?"
+      },
+      {
         type: "input",
         name: "licenses",
         message: "What licenses will your program have?"
       },
-    {
+      {
         type: "input",
         name: "Contributing",
         message: "What does the user need to know about contributing to your repository?"
@@ -52,18 +70,30 @@ function promptUser() {
         message: "If the user has questions how can they reach you?"
       },
     ]);
+  } catch (err) {
+    console.log(err);
   }
+}
 
-  //generate a basic txt file 
-
+ promptUser()
+.then(function(response) {
+  console.log(response);
   
-writeFileAsync("test.txt", "utf8")
-  .then((data) => {
-    console.log(data);
-})
-  .catch((err) => {
-      console.log(err);
-  })  
+});
+
+
+
+
+  //generate a basic txt file
+
+
+// writeFileAsync("test.txt", "utf8")
+//   .then((data) => {
+//     console.log(data);
+// })
+//   .catch((err) => {
+//       console.log(err);
+//   })  
 
 ///create layout of readme file, insert user input 
 //generate a badge. 
